dda0788ced219e5cc935948033bce3dec882ca2b
Kailany Alves <119695136+kailanyas@users.noreply.github.com>
2023-10-18 18:20:17 -0300

    Create Impares.java

---
+++ NumerosImpares/Impares.java
@@ -0,0 +1,13 @@
+public class Impares{
+    public static void main(String[] args){
+        int i = 0;
+        int max = 15;
+        
+        for(i = 0; i <= max; i++){
+            if((i % 2) != 0){
+                System.out.printf("%d ", i);
+            }
+        }
+    }
+
+}
