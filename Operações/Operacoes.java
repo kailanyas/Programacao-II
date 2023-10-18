75d50962e21794af5dfdd9d22843287b5419b7f2
Kailany Alves <119695136+kailanyas@users.noreply.github.com>
2023-10-18 18:18:34 -0300

    Create Operacoes.java

---
+++ Operações/Operacoes.java
@@ -0,0 +1,16 @@
+public class Operacoes{
+    
+    public static void main(String[] args){
+        float resul1;
+        float resul2;
+        float resul3;
+        
+        resul1 = (-5 + 7 * 8);
+        resul2 = (72 + 9 % 9);
+        resul3 = (5 + ((15/3) * 3) - 4 % 3);
+    
+        System.out.printf("I: %.2f\nII: %.2f\nIII: %.2f\n", resul1, resul2, resul3);
+        
+    }
+
+}
