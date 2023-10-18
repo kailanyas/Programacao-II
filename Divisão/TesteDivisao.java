602c0c634680dd7d610ed387d203aaf921ab8bb7
Kailany Alves <119695136+kailanyas@users.noreply.github.com>
2023-10-18 18:17:52 -0300

    Create TesteDivisao.java

---
+++ Divisão/TesteDivisao.java
@@ -0,0 +1,10 @@
+public class TesteDivisao{
+    public static void main(String[] args){
+        Divisao r = new Divisao();
+        
+        r.setResto(10, 2);
+        
+        System.out.println("Resto: " + r.getResto());
+    }
+
+}
